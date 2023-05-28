925fc5181cbe4817bad1ea02e5a4cd77ee94a7be
Denis Rwelamila <denis@ramani.io>
2023-05-28 20:32:03 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f05a84a35efb9b4bf2e9f21f02845423.js"
+  "/precache-manifest.017bdc2db785db66d438fdcf3361f300.js"
 );
 
 workbox.clientsClaim();
